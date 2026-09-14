ed05eb90122e6fdcde0486fbdff67f570bf7a7c3
Valentin Aurich <valentin.aurich@googlemail.com>
2026-09-14 19:48:33 +0200

    T statt omega

--- V2_Mechanik/Valentin/auswertung_valentin.py
+++ V2_Mechanik/Valentin/auswertung_valentin.py
@@ -40,8 +40,8 @@ def Periodebestimmen(DATEN, messreihe):
     t = messung.datenreihe('t').werte[10:]
     U = messung.datenreihe('U_B1').werte[10:]
     
-    def f(t, A, B, w, phi, y_0):
-        return A * np.exp(-B * t) * np.cos(w * t + phi) + y_0
+    def f(t, A, B, T, phi, y_0):
+        return A * np.exp(-B * t) * np.cos(2* np.pi / T * t + phi) + y_0
         
     popt, _ = curve_fit(f, t, U, p0=[U.max(), 0.1, 2 * np.pi, 0, np.mean(U)])
     
@@ -78,14 +78,14 @@ def auswertung_messreihe(DATEN, i, rausch_sigma=None, fit_trim=[0,-1], plot_inte
     t = messung.datenreihe('t').werte[fit_trim[0]:fit_trim[1]]
     U = messung.datenreihe('U_B1').werte[fit_trim[0]:fit_trim[1]]
 
-    def f(t, A, B, w, phi, y_0):
-        return A * np.exp(-B * t) * np.cos(w * t + phi) + y_0
+    def f(t, A, B, T, phi, y_0):
+        return A * np.exp(-B * t) * np.cos(2 * np.pi / T * t + phi) + y_0
     
     popt, pcov = curve_fit(f, t, U, p0=[U.max(), 0.1, 2 * np.pi, 0, np.mean(U)], sigma=rausch_sigma, absolute_sigma=True)
-    werr = np.sqrt(pcov[2,2])
+    Terr = np.sqrt(pcov[2,2])
     
-    A_fit, B_fit, w_fit, phi_fit, y_0_fit = popt
-    T_fit = 2 * np.pi / w_fit
+    A_fit, B_fit, T_fit, phi_fit, y_0_fit = popt
+    w_fit = 2 * np.pi / T_fit
     
     fit = f(t, *popt)
     
@@ -97,8 +97,8 @@ def auswertung_messreihe(DATEN, i, rausch_sigma=None, fit_trim=[0,-1], plot_inte
     
     # print(f'U_0 = {A_fit:.4f}')
     # print(f'delta = {B_fit:.4f}')
-    print(f'w = ({w_fit:.4f}+-{werr})')
-    print(f'T = {T_fit:.4f}')
+    print(f'w = ({w_fit:.4f})')
+    print(f'T = ({T_fit:.4f} +/- {Terr})')
     # print(f'phi = {phi_fit:.4f}')
     # print(f'y_0 = {y_0_fit:.4f}')
     print(f'Chiq / dof = {chiq/dof}')
@@ -109,10 +109,12 @@ def auswertung_messreihe(DATEN, i, rausch_sigma=None, fit_trim=[0,-1], plot_inte
         figsize=(10,7),
         constrained_layout=True
     )
-    ax.plot(
+    ax.errorbar(
         t[plot_intervall[0]:plot_intervall[1]],
         U[plot_intervall[0]:plot_intervall[1]],
-        marker='o',
+        fmt='',
+        capsize=3,
+        yerr=rausch_sigma,
         ls='',
         label=f'Messreihe {i}',
         color='tab:blue',
@@ -153,7 +155,7 @@ def auswertung_messreihe(DATEN, i, rausch_sigma=None, fit_trim=[0,-1], plot_inte
     rs.legend(loc='upper right')
     fig.savefig(OUTPUT / f'MessungPlusFit{i}', dpi=300, bbox_inches='tight')
     plt.close(fig)
-    return w_fit, chiq/dof, werr
+    return T_fit, chiq/dof, Terr
 
 perioden_om   = []
 ergebnisse_om = []
@@ -169,8 +171,8 @@ for DATEN in DATEN_Pendel_mit_Masse:
     ergebnisse_mm.append(ergebnis)
     perioden_mm.append(ergebnis[2])
 
-def f(t, A, B, w, phi, y_0):
-        return A * np.exp(-B * t) * np.cos(w * t + phi) + y_0
+def f(t, A, B, T, phi, y_0):
+        return A * np.exp(-B * t) * np.cos(2 * np.pi / T * t + phi) + y_0
     
 fig, ax = plt.subplots(figsize=(10,7), constrained_layout=True)
 t = CassyDaten(DATEN_Periodenangleich_om_1).messung(1).datenreihe('t').werte
@@ -228,7 +230,8 @@ print(l1)
 print(l2)
 print(d_p)
 print(l_p)
-omegas = []
+
+Ts = []
 chiqs  = []
 g_vals = []
 g_errs = []
@@ -236,21 +239,21 @@ for i in range(1, 11):
 
     fit_daten = auswertung_messreihe(DATEN_Messreihen, i, plot_intervall=[3000,3500], fit_trim=[trim_vorne,-1], rausch_sigma=sigma_ges)
 
-    w = un.ufloat(fit_daten[0], fit_daten[2])
-    g = w**2 * (l_p / 1000) * (1 + (1/8) * ((d_p/1000) / (l_p/1000))**2)
+    T = un.ufloat(fit_daten[0], fit_daten[2])
+    g = 4 * np.pi**2 / T**2 * (l_p / 1000) * (1 + (1/8) * ((d_p/1000) / (l_p/1000))**2)
     
     print(g)
     
-    omegas.append(fit_daten[0])
+    Ts.append(fit_daten[0])
     chiqs.append(fit_daten[1])
     g_vals.append(g.n)
     g_errs.append(g.s)
 
-omega_mean = np.mean(omegas)
-omega_std  = np.std(omegas, ddof=1)
+T_mean = np.mean(Ts)
+T_std  = np.std(Ts, ddof=1)
 
-# g aus gemitteltem omega
-g_mean = (omega_mean**2 * (l_p.n / 1000) * (1 + (1/8) * ((d_p.n / 1000) / (l_p.n / 1000))**2))
+# g aus gemitteltem T
+g_mean = (4 * np.pi**2 / T_mean**2 * (l_p.n / 1000) * (1 + (1/8) * ((d_p.n / 1000) / (l_p.n / 1000))**2))
 
 # Fehler aus Streuung der Messreihen
 g_std = np.std(g_vals, ddof=1)
@@ -260,7 +263,7 @@ g_stat = g_std / np.sqrt(len(g_vals))
 g_fit = (np.sqrt(np.sum(np.array(g_errs)**2)) / len(g_errs))
 
 # Gemeinsamer Fehler von l_p und d_p
-g_l_d = (un.ufloat(omega_mean, 0)**2 * (l_p / 1000) * (1 + (1/8) * ((d_p/1000) / (l_p/1000))**2))
+g_l_d = (4 * np.pi**2 / T_mean**2 * (l_p / 1000) * (1 + (1/8) * ((d_p/1000) / (l_p/1000))**2))
 
 g_l_d_err = g_l_d.s
 
@@ -268,7 +271,7 @@ g_l_d_err = g_l_d.s
 g_ges = np.sqrt(g_stat**2 + g_fit**2 + g_l_d_err**2)
 
 # Ausgabe
-print(f'\nomega = ({omega_mean:.4f} +/- {omega_std:.4f}) 1/s')
+print(f'\nT = ({T_mean:.4f} +/- {T_std:.4f}) s')
 
 print(f'g_mean = {g_mean:.6f} m/s²')
 print(f'g_stat = {g_stat:.6f} m/s²')
